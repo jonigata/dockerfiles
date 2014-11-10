a618cd3b00c565c2f81aeeda14975a506062411c
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2014-11-10 18:49:00 +0900

    use relative directory as a volume

--- h2opp/docker_run.sh
+++ h2opp/docker_run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-docker run -it --name $* -v /home/hirayama/docker/h2opp/volume:/home/hirayama/work jonigata/h2o:1.0 /bin/bash
+if [ $# -ne 1 ]; then
+	echo "usage: $0 container-name" 1>& 2
+	exit 1
+fi
+
+DIR=$(cd $(dirname $0); pwd)
+echo $DIR/volume
+
+docker run -it --name $* -v ${DIR}/volume:/home/hirayama/work jonigata/h2o:1.0 /bin/bash
